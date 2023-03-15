1306d8e17d94623597a9d18cd160903aceb8baf6
joeyissa <joeyissa59@gmail.com>
2023-03-15 10:23:36 -0400

    Char Selection scripiting started

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,27 @@
 //Track mouse movement 
+
+function myFunction(character) {
+
+    if (character == Paimon) {
+
+        //set the image to the characterr
+
+    }
+
+    if (character == Primogem) {
+        
+    }
+
+    if (character == Venti) {
+        
+    }
+    
+    if (character == Zhongli) {
+        
+    }
+    
+    if (character == Raiden) {
+        
+    }
+
+}
