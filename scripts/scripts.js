4602e7a26552b2811b94048ef853baa11a249e40
joeyissa <joeyissa59@gmail.com>
2023-03-15 15:43:21 -0400

    gameplan mapped out

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,25 @@
-$(document).ready(function() {
-  var init = true;
-  var which="";
-  $(document).on('click', function(event) {
-      $(this)[init?'on':'off']('mousemove', follow);
-      init = !init;
-      which = event.target.id;
-  });
+function myMove() {
+    let id = null;
+    const elem = document.getElementById("image");   
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 350) {
+        clearInterval(id);
+      } else {
+        pos++; 
+        elem.style.top = pos + "px"; 
+        elem.style.left = pos + "px"; 
+      }
+    }
+}
 
-  function follow(e) {
-  console.log(which);
-      var xPos = e.pageX;
-      var yPos = e.pageY;
-     $("#imgLocation").html("The img is at: " + xPos + ", " + yPos);
-      $("#"+which).offset({
-          left: e.pageX,
-          top: e.pageY
-      });
-  }
-});
+
+//for scripts
+//get postion where cursor is when there is a specific key click
+//make image travel to that spot 
+
+//for content
+//somehow change the img src directory location so that it points to a specific 
+//img on button click
