89bba48f3e23a500a227d486c57fad57436ab4fd
joeyissa <joeyissa59@gmail.com>
2023-03-17 15:16:11 -0400

    new image for testing

--- scripts/content.js
+++ scripts/content.js
@@ -1,38 +1,61 @@
-//Track mouse movement 
-document.getElementById("Paimon").addEventListener("click", changeImg(Paimon));
-document.getElementById("Primo").addEventListener("click", changeImg(Primogem));
-document.getElementById("Venti").addEventListener("click", changeImg(Venti));
-document.getElementById("Zhong").addEventListener("click", changeImg(Zhongli));
-document.getElementById("Raiden").addEventListener("click", changeImg(Raiden));
-document.getElementById("Nahida").addEventListener("click", changeImg(Nahida));
+const Paimon = document.getElementById("Paimon1");
+const Primo = document.getElementById("Primo1");
+const Venti = document.getElementById("Venti1");
+const Zhong = document.getElementById("Zhong1");
+const Raiden = document.getElementById("Raiden1");
+const Nahida = document.getElementById("Nahida1");
+
+if (Paimon) {
+    Paimon.addEventListener("click", changeImg('Paimon'));
+}
+if (Primo) {
+    Primo.addEventListener("click", changeImg('Primo'));
+}
+if (Venti) {
+    Venti.addEventListener("click", changeImg('Venti'));;
+}
+if (Zhong) {
+    Zhong.addEventListener("click", changeImg('Zhong'));
+}
+if (Raiden) {
+    Raiden.addEventListener("click", changeImg('Raiden'));
+}
+if (Nahida) {
+    Nahida.addEventListener("click", changeImg('Nahida'));
+}
+
+
+
 
 function changeImg(string) {
 
-    if (string == Paimon) {
-        document.getElementById('character').src='/GenshinExtensionSprites/test.png';
+    if (string == "Paimon") {
+       // document.getElementById('character').src='/GenshinExtensionSprites/test.png';
+        document.body.style.cursor = '/GenshinExtensionSprites/test.png';
     }
 
-    else if (string == Primogem) {
+    else if (string == "Primogem") {
         
-        document.getElementById('character').src ='/GenshinExtensionSprites/test.png';
+        //document.getElementById('character').src ='/GenshinExtensionSprites/test.png';
+        document.body.style.cursor = '/GenshinExtensionSprites/test.png';
     }
 
-    else if (string == Venti) {
+    else if (string == "Venti") {
 
-        document.getElementById('character').src ='/GenshinExtensionSprites/128.png';
-        
-    }
-    
-    else if (string == Zhongli) {
-
-        document.getElementById('character').src ='/GenshinExtensionSprites/test.png';
-        
+       // document.getElementById('character').src ='/GenshinExtensionSprites/128.png';
+       document.body.style.cursor = '/GenshinExtensionSprites/test.png';
     }
     
-    else if (string == Raiden) {
+    else if (string == "Zhong") {
 
-        document.getElementById('character').src ='/GenshinExtensionSprites/test.png';
-        
+        //document.getElementById('character').src ='/GenshinExtensionSprites/test.png';
+        document.body.style.cursor = '/GenshinExtensionSprites/test.png';
+    }
+    
+    else if (string == "Raiden") {
+
+       // document.getElementById('character').src ='/GenshinExtensionSprites/raiden.png';
+       document.body.style.cursor = '/GenshinExtensionSprites/raiden.png';
     }
 
 }
