1bf46cf4c8144874c9e2e773cbf8f9fd792b096a
joeyissa <joeyissa59@gmail.com>
2023-03-18 11:09:34 -0400

    updating code for future storage capabilities

--- scripts/background.js
+++ scripts/background.js
@@ -1,1 +1,19 @@
+chrome.runtime.onMessage.addListerner(data => {
+    switch (data.event) {
+        case "Paimon":
+            console.log("Change Background");
+        case "Childe":
+            console.log("Change Background");
+        case "Venti":
+            console.log("Change Background");
+        case "Zhong":
+            console.log("Change Background");
+        case "Raiden":
+            console.log("Change Background");
+        case "Nah":
+            console.log("Change Background");
+                    
+    }
+})
 
+//chrome.storage.local.set()
--- scripts/popup.js
+++ scripts/popup.js
@@ -7,26 +7,40 @@ const Raiden = document.getElementById("Raiden1");
 const Nahida = document.getElementById("Nahida1");
 
 Paimon.onclick = function() {
+    chrome.runtime.sendMessage({event: "Paimon"})
     console.log("You Clicked Paimon")
     document.getElementById('character').src='/GenshinExtensionSprites/paimon.png';
 }
 Childe.onclick = function() {
+    chrome.runtime.sendMessage({event: "Childe"})
     console.log("You Clicked Childe")
     document.getElementById('character').src='/GenshinExtensionSprites/childe.png';
 }
 Venti.onclick = function() {
+    chrome.runtime.sendMessage({event: "Venti"})
     console.log("You Clicked Venti")
     document.getElementById('character').src='/GenshinExtensionSprites/venti.png';
 }
 Zhong.onclick = function() {
+    chrome.runtime.sendMessage({event: "Zhong"})
     console.log("You Clicked Zhong")
     document.getElementById('character').src='/GenshinExtensionSprites/zhongli.png';
 }
 Raiden.onclick = function() {
+    chrome.runtime.sendMessage({event: "Raiden"})
     console.log("You Clicked Raiden")
     document.getElementById('character').src='/GenshinExtensionSprites/raiden.png';
 }
 Nahida.onclick = function() {
+    chrome.runtime.sendMessage({event: "Nah"})
     console.log("You Clicked Nahida")
     document.getElementById('character').src='/GenshinExtensionSprites/nah.png';
 }
+
+/*chrome.storage.local.get(["selection"], (result) => {
+    const {selection} = result;
+
+    if (selection) {
+        
+    }
+}) */
