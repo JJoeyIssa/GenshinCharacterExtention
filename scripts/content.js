99f89fdef76c186d67be2df08b87bb3dea3ab825
joeyissa <joeyissa59@gmail.com>
2023-03-20 12:41:54 -0400

    Annoying scroll bar on extension removed

--- scripts/content.js
+++ scripts/content.js
@@ -3,5 +3,13 @@ img.id = "overlay";
 img.className = "overlay";
 */
 
+//remove scroll bar on extension
+var styleElement = document.createElement('style');
+    styleElement.id = 'remove-scroll-style';
+    styleElement.textContent =
+        'html::-webkit-scrollbar{display:none !important}' +
+        'body::-webkit-scrollbar{display:none !important}';
+    document.getElementsByTagName('body')[0].appendChild(styleElement);
+
 
 //inject element (Image) onto page
