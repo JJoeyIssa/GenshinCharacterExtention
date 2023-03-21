83fa8d30a8b484245c29432631a1d86718429e72
joeyissa <joeyissa59@gmail.com>
2023-03-21 17:09:12 -0400

    updating scripts

--- scripts/background.js
+++ scripts/background.js
@@ -1,19 +1,26 @@
-/*chrome.runtime.onMessage.addListerner(data => {
+chrome.runtime.onMessage.addListerner(data => {
     switch (data.event) {
         case "Paimon":
             console.log("Change Background");
+            document.getElementById('overlay').src='/GenshinExtensionSprites/paimon.png';
         case "Childe":
             console.log("Change Background");
+            document.getElementById('overlay').src='/GenshinExtensionSprites/childe.png';
         case "Venti":
             console.log("Change Background");
+            document.getElementById('overlay').src='/GenshinExtensionSprites/venti.png';
         case "Zhong":
             console.log("Change Background");
+            document.getElementById('overlay').src='/GenshinExtensionSprites/zhongli.png';
         case "Raiden":
             console.log("Change Background");
+            document.getElementById('overlay').src='/GenshinExtensionSprites/raiden.png';
         case "Nah":
             console.log("Change Background");
+            document.getElementById('overlay').src='/GenshinExtensionSprites/nah.png';
                     
     }
 })
-*/
+
+
 //chrome.storage.local.set()
