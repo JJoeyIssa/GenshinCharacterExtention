923105892ad68e9a6734e76cd400b304b387a7b5
joeyissa <joeyissa59@gmail.com>
2023-03-22 20:27:26 -0400

    updated overlay to sit center

--- scripts/inject.js
+++ scripts/inject.js
@@ -3,8 +3,8 @@
 	// just place a div at top right
 	var img = document.createElement('img');
 	img.style.position = 'fixed';
-	img.style.top = 0;
-	img.style.right = 0;
+	//img.style.top = 0;
+	//img.style.right = 0;
     img.className = "overlay";
 	img.src = "/GenshinExtensionSprites/nah.png";
 	document.body.appendChild(img);
